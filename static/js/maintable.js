e1629168c2be17ffba3e237a8fedd81d87e5c64f
Congb19 <347006087@qq.com>
2019-04-27 14:46:03 +0800

    4.27 add maintable

---
+++ static/js/maintable.js
@@ -0,0 +1,51 @@
+$('#mytab').bootstrapTable({
+    url: "../static/js/tableconfig.json",
+    queryParams: "queryParams",
+    toolbar: "#toolbar",
+    sidePagination: "true",
+    striped: true, // 是否显示行间隔色
+    search : "true",
+    uniqueId: "ID",
+    pageSize: "8",
+    pagination: false, // 是否分页
+    // pageList: [5, 8, 11, 14],        //可供选择的每页的行数（*）
+    sortable: true, // 是否启用排序
+    cardView: false,                    //是否显示详细视图
+    showColumns: true,                  //是否显示所有的列
+    showRefresh: true,                  //是否显示刷新按钮
+    columns: [{
+        field: 'id',
+        title: '编号'
+    },
+        {
+            field: 'time',
+            title: '时间'
+        },
+        {
+            field: 'type',
+            title: '类型'
+        },
+        {
+            field: 'content',
+            title: '信件内容'
+        },
+        {
+            field: 'price',
+            title: '操作',
+            width: 120,
+            align: 'center',
+            valign: 'middle',
+            formatter: actionFormatter,
+        },
+
+    ]
+});
+//操作栏的格式化
+function actionFormatter(value, row, index) {
+    var id = value;
+    var result = "";
+    result += "<a href='javascript:;' class='btn btn-xs btn-info' onclick=findsimilar() title='查看相似事件'  data-toggle=\"modal\" data-target=\".bs-similar-modal-lg\"><span class='glyphicon glyphicon-search'></span></a>&nbsp;";
+    result += "<a href='javascript:;' class='btn btn-xs btn-warning' onclick=chakanxiangqing() title='查看详情/回复'  data-toggle=\"modal\" data-target=\".bs-example-modal-lg\"><span class='glyphicon glyphicon-pencil'></span></a>&nbsp;";
+    // result += "<a href='javascript:;' class='btn btn-xs btn-danger' onclick=\"DeleteByIds('" + id + "')\" title='退回'><span class='glyphicon glyphicon-remove'></span></a>";
+    return result;
+}
